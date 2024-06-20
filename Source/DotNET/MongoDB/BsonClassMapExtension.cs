9d822cfe8aac7c48c71fcda606370cea1bee4da8
Einar Ingebrigtsen <einari@me.com>
2024-06-20 14:43:09 +0200

    Adding support for unmapping everything except and also automatically applying conventions

---
+++ Source/DotNET/MongoDB/BsonClassMapExtension.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq.Expressions;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace Cratis.MongoDB;
+
+/// <summary>
+/// Extension methods for working with <see cref="BsonClassMap{T}"/>.
+/// </summary>
+public static class BsonClassMapExtension
+{
+    /// <summary>
+    /// Unmaps all members except the ones specified.
+    /// </summary>
+    /// <param name="classMap"><see cref="BsonClassMap{T}"/> to unmap from.</param>
+    /// <param name="membersToKeep">Members to keep represented as lambdas.</param>
+    /// <typeparam name="T">Type of class the class map is for.</typeparam>
+    public static void UnmapAllMembersExcept<T>(this BsonClassMap<T> classMap, params Expression<Func<T, object>>[] membersToKeep)
+    {
+        var membersToKeepNames = membersToKeep.Select(_ => ((MemberExpression)_.Body).Member.Name.ToLowerInvariant()).ToArray();
+        foreach (var member in classMap.DeclaredMemberMaps.Where(_ => !membersToKeepNames.Contains(_.MemberName.ToLowerInvariant())).ToArray())
+        {
+            classMap.UnmapMember(member.MemberInfo);
+        }
+    }
+
+    /// <summary>
+    /// Apply conventions to a <see cref="BsonClassMap{T}"/>.
+    /// </summary>
+    /// <param name="classMap"><see cref="BsonClassMap{T}"/> to apply to.</param>
+    /// <typeparam name="T">Type of class the class map is for.</typeparam>
+    public static void ApplyConventions<T>(this BsonClassMap<T> classMap)
+    {
+        foreach (var convention in ConventionRegistry.Lookup(typeof(T)).Conventions.OfType<IMemberMapConvention>())
+        {
+            foreach (var member in classMap.DeclaredMemberMaps)
+            {
+                convention.Apply(member);
+            }
+        }
+    }
+}
---
+++ Specifications/MongoDB/for_BsonClassMapExtensions/when_unmapping_all_except/and_members_one_wants_to_keep_are_camel_cased.cs
@@ -0,0 +1,27 @@
+using Cratis.Strings;
+using MongoDB.Bson.Serialization;
+
+namespace Cratis.MongoDB.for_BsonClassMapExtensions.when_unmapping_all_except;
+
+public class and_members_one_wants_to_keep_are_camel_cased : Specification
+{
+    class SomeType
+    {
+        public string SomeProperty { get; init; }
+        public string SomeOtherProperty { get; init; }
+    }
+
+    BsonClassMap<SomeType> class_map;
+
+    void Establish()
+    {
+        class_map = new BsonClassMap<SomeType>();
+        class_map.MapMember(_ => _.SomeProperty).SetElementName(nameof(SomeType.SomeProperty).ToCamelCase());
+        class_map.MapMember(_ => _.SomeOtherProperty).SetElementName(nameof(SomeType.SomeOtherProperty).ToCamelCase());
+    }
+
+    void Because() => class_map.UnmapAllMembersExcept(_ => _.SomeOtherProperty);
+
+    [Fact] void should_have_one_member() => class_map.DeclaredMemberMaps.Count().ShouldEqual(1);
+    [Fact] void should_have_the_correct_member() => class_map.DeclaredMemberMaps.First().ElementName.ShouldEqual(nameof(SomeType.SomeOtherProperty).ToCamelCase());
+}
