b46f0edb07961386cbfffc5ce9eb632ade130f83
Einar Ingebrigtsen <einari@me.com>
2024-07-01 08:15:20 +0200

    Adding order by support

--- Source/DotNET/Applications/Queries/QueryableExtensions.cs
+++ Source/DotNET/Applications/Queries/QueryableExtensions.cs
@@ -14,20 +14,30 @@ public static class QueryableExtensions
     static readonly MethodInfo _countMethod;
     static readonly MethodInfo _skipMethod;
     static readonly MethodInfo _takeMethod;
+    static readonly MethodInfo _orderByMethod;
+    static readonly MethodInfo _orderByDescendingMethod;
 
     static QueryableExtensions()
     {
         var queryableMethods = typeof(Queryable).GetTypeInfo().GetMethods(BindingFlags.Public | BindingFlags.Static);
 
-        _countMethod = queryableMethods.First(m => m.Name == "Count" && m.GetParameters().Length == 1);
-        _skipMethod = queryableMethods.First(m => m.Name == "Skip");
-        _takeMethod = queryableMethods.First(m => m.Name == "Take");
+        _countMethod = queryableMethods.First(m => m.Name == nameof(Queryable.Count) && m.GetParameters().Length == 1);
+        _skipMethod = queryableMethods.First(m => m.Name == nameof(Queryable.Skip));
+        _takeMethod = queryableMethods.First(m => m.Name == nameof(Queryable.Take));
+        _orderByMethod = queryableMethods.First(
+            m => m.Name == nameof(Queryable.OrderBy) &&
+            m.GetParameters().Length == 2 &&
+            m.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Expression<>));
+        _orderByDescendingMethod = queryableMethods.First(
+            m => m.Name == nameof(Queryable.OrderByDescending) &&
+            m.GetParameters().Length == 2 &&
+            m.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Expression<>));
     }
 
     /// <summary>
     /// Returns the number of elements in a sequence.
     /// </summary>
-    /// <param name="queryable">The <see cref="IQueryable"/> that contains the elements to be counted.</param>
+    /// <param name="queryable">The <see cref="IQueryable"/> to adorn.</param>
     /// <returns>The number of elements in the input sequence.</returns>
     public static int Count(this IQueryable queryable)
     {
@@ -39,9 +49,9 @@ public static class QueryableExtensions
     /// <summary>
     /// Bypasses a specified number of elements in a sequence and then returns the remaining elements.
     /// </summary>
-    /// <param name="queryable">An <see cref="IQueryable"/> to return elements from.</param>
+    /// <param name="queryable">An <see cref="IQueryable"/> to adorn.</param>
     /// <param name="count">The number of elements to skip before returning the remaining elements.</param>
-    /// <returns>An <see cref="IQueryable"/> that contains elements that occur after the specified index in the input sequence.</returns>
+    /// <returns>An <see cref="IQueryable"/> for continuation.</returns>
     public static IQueryable Skip(this IQueryable queryable, int count)
     {
         var genericMethod = _skipMethod.MakeGenericMethod([queryable.ElementType]);
@@ -51,12 +61,30 @@ public static class QueryableExtensions
     /// <summary>
     /// Returns a specified number of contiguous elements from the start of a sequence.
     /// </summary>
-    /// <param name="queryable">The <see cref="IQueryable"/> to return elements.</param>
+    /// <param name="queryable">The <see cref="IQueryable"/> to adorn.</param>
     /// <param name="count">The number of elements to return.</param>
-    /// <returns>An <see cref="IQueryable"/> that contains the specified number of elements from the start of source.</returns>
+    /// <returns>An <see cref="IQueryable"/> for continuation.</returns>
     public static IQueryable Take(this IQueryable queryable, int count)
     {
         var genericMethod = _takeMethod.MakeGenericMethod([queryable.ElementType]);
         return (genericMethod.Invoke(null, [queryable, count]) as IQueryable)!;
+    }
+
+    /// <summary>
+    /// Sorts the elements of a sequence in ascending or descending order according to a key.
+    /// </summary>
+    /// <param name="queryable">The <see cref="IQueryable"/> to adorn.</param>
+    /// <param name="field">The name of the field to order on.</param>
+    /// <param name="direction">Optional direction of sort. Defaults to ascending.</param>
+    /// <returns>An <see cref="IQueryable"/> for continuation.</returns>
+    public static IQueryable OrderBy(this IQueryable queryable, string field, SortDirection direction = SortDirection.Ascending)
+    {
+        var orderMethod = direction == SortDirection.Ascending ? _orderByMethod : _orderByDescendingMethod;
+
+        var parameter = Expression.Parameter(queryable.ElementType, "x");
+        var property = Expression.Property(parameter, field);
+        var lambda = Expression.Lambda(property, parameter);
+
+        return (orderMethod.Invoke(null, [queryable, lambda]) as IQueryable)!;
     }
 }
