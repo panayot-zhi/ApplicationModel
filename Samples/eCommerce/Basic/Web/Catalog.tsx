d2a68a35d13125762b1fc2578a0933b93a654a8e
Einar Ingebrigtsen <einari@me.com>
2024-07-04 10:20:11 +0200

    Adding paging to sample

--- Samples/eCommerce/Basic/Web/Catalog.tsx
+++ Samples/eCommerce/Basic/Web/Catalog.tsx
@@ -6,16 +6,25 @@ import { CatalogViewModel } from './CatalogViewModel';
 import { AllProducts } from './API/Products';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { useState } from 'react';
 
 export const Catalog = withViewModel(CatalogViewModel, ({ viewModel }) => {
-    const [products] = AllProducts.useWithPaging(0, 10);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [products, perform, setSorting, setPage] = AllProducts.useWithPaging(currentPage, 10);
 
     return (
         <div>
+            <div>Page {currentPage}</div>
             <DataTable value={products.data}>
-                <Column field="SKU" header="SKU" />
+                <Column field="id" header="SKU" />
                 <Column field="name" header="Name" />
             </DataTable>
+
+            <button onClick={() => {
+                const page = currentPage + 1;
+                setCurrentPage(page);
+                setPage(page);
+            }}>Next page</button>
         </div>
     );
 });
