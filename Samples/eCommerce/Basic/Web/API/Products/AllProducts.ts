6168b187c6cfedfb9c45e2fab07cfc95e7b6009d
woksin <sindrewilting@gmail.com>
2024-07-05 03:07:42 +0200

    Temporarily remove the custom sorting stuff from handlebars, it will not work

--- Samples/eCommerce/Basic/Web/API/Products/AllProducts.ts
+++ Samples/eCommerce/Basic/Web/API/Products/AllProducts.ts
@@ -10,37 +10,14 @@ import Handlebars from 'handlebars';
 
 const routeTemplate = Handlebars.compile('/api/products/catalog');
 
-class AllProductsSortBy {
-    private _id: SortingForQuery<AllProducts>;
-    private _name: SortingForQuery<AllProducts>;
-    private _isRegistered: SortingForQuery<AllProducts>;
-
-    constructor(readonly query: AllProducts) {
-        this._id = new SortingForQuery(AllProducts, query);
-        this._name = new SortingForQuery(AllProducts, query);
-        this._isRegistered = new SortingForQuery(AllProducts, query);
-    }
-
-    id(): SortingForQuery<AllProducts> {
-        return this._id;
-    }
-    name(): SortingForQuery<AllProducts> {
-        return this._name;
-    }
-    isRegistered(): SortingForQuery<AllProducts> {
-        return this._isRegistered;
-    }
-}
 
 export class AllProducts extends QueryFor<Product[]> {
     readonly route: string = '/api/products/catalog';
     readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
     readonly defaultValue: Product[] = [];
-    readonly _sortBy: AllProductsSortBy;
 
     constructor() {
         super(Product, true);
-        this._sortBy = new AllProductsSortBy(this);
     }
 
     get requestArguments(): string[] {
@@ -48,15 +25,12 @@ export class AllProducts extends QueryFor<Product[]> {
         ];
     }
 
-    get sortBy(): AllProductsSortBy {
-        return this._sortBy;
-    }
 
     static use(): [QueryResultWithState<Product[]>, PerformQuery, SetSorting] {
         return useQuery<Product[], AllProducts>(AllProducts);
     }
 
-    static useWithPaging(pageSize: number): [QueryResultWithState<Product[]>, number, PerformQuery, SetSorting, SetPage] {
-        return useQueryWithPaging<Product[], AllProducts>(AllProducts, new Paging(0, pageSize));
+    static useWithPaging(page:number, pageSize: number): [QueryResultWithState<Product[]>, number, PerformQuery, SetSorting, SetPage] {
+        return useQueryWithPaging<Product[], AllProducts>(AllProducts, new Paging(page, pageSize));
     }
 }
