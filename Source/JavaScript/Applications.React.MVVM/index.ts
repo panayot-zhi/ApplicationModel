0331bb5799958d29a3805b02dca4f0a292b39e48
Einar Ingebrigtsen <einari@me.com>
2024-07-29 10:50:24 +0200

    Fixing import for Bindings - its not a module

--- Source/JavaScript/Applications.React.MVVM/index.ts
+++ Source/JavaScript/Applications.React.MVVM/index.ts
@@ -2,14 +2,14 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import 'reflect-metadata';
-import * as bindings from './Bindings';
 import * as browser from './browser';
 import * as messaging from './messaging';
+import { Bindings } from './Bindings';
 import { withViewModel } from './withViewModel';
 import { MVVM, MVVMContext, MVVMProps } from './MVVMContext';
 
 export {
-    bindings,
+    Bindings,
     browser,
     messaging,
     withViewModel,
