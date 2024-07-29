3625d16c29dd936842b81152ee958b1f5d92b057
Einar Ingebrigtsen <einari@me.com>
2024-07-29 09:40:02 +0200

    Export bindings

--- Source/JavaScript/Applications.React.MVVM/index.ts
+++ Source/JavaScript/Applications.React.MVVM/index.ts
@@ -2,12 +2,14 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import 'reflect-metadata';
+import * as bindings from './Bindings';
 import * as browser from './browser';
 import * as messaging from './messaging';
 import { withViewModel } from './withViewModel';
 import { MVVM, MVVMContext, MVVMProps } from './MVVMContext';
 
 export {
+    bindings,
     browser,
     messaging,
     withViewModel,
