032a85cd2f608a8482deb2e4ddc7822d345495df
Einar Ingebrigtsen <einari@me.com>
2024-08-27 14:00:36 +0200

    Adding a [Roles] attribute for specifying roles in a more meaningful way

---
+++ Source/DotNET/Applications/Authorization/RolesAttribute.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cratis.Applications.Authorization;
+
+/// <summary>
+/// Represents an <see cref="AuthorizeAttribute"/> for specifying roles.
+/// </summary>
+public sealed class RolesAttribute : AuthorizeAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RolesAttribute"/> class.
+    /// </summary>
+    /// <param name="roles">Roles that is needed.</param>
+    public RolesAttribute(params string[] roles)
+    {
+        Roles = string.Join(',', roles);
+    }
+}
