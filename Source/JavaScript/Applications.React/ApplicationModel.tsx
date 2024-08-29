542f0cbb8e07f3785673cbb063b1d6ebbc059489
Einar Ingebrigtsen <einari@me.com>
2024-08-29 18:57:35 +0200

    Fixing references to CommandScope and IdentityProvider

--- Source/JavaScript/Applications.React/ApplicationModel.tsx
+++ Source/JavaScript/Applications.React/ApplicationModel.tsx
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Globals } from '@cratis/applications';
-import { CommandScope } from 'commands';
-import { IdentityProvider } from 'identity';
+import { CommandScope } from './commands';
+import { IdentityProvider } from './identity';
 import React from 'react';
 
 export interface ApplicationModelProps {
