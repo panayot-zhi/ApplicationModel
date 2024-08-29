e461a92e239d67ea40c357543e157cb2d1772314
Einar Ingebrigtsen <einari@me.com>
2024-08-29 19:10:47 +0200

    Fixing so that we pass the specs that should be removed when we have proper ones

--- Source/JavaScript/Applications.React.MVVM/for_RemoveMeSpec/when_actual_spec_added.ts
+++ Source/JavaScript/Applications.React.MVVM/for_RemoveMeSpec/when_actual_spec_added.ts
@@ -2,5 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 describe('when actual spec added', () => {
-    it('should be removed', () => true.should.be.true);
+    it('should be removed', () => true);
 });
