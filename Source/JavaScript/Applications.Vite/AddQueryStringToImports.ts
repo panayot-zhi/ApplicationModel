6176d1f97c54952bf196aef0f3c4bda0b9d8f6e3
Einar Ingebrigtsen <einari@me.com>
2024-09-02 18:58:42 +0200

    Plugin for adding query string to imports

---
+++ Source/JavaScript/Applications.Vite/AddQueryStringToImports.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export const AddQueryStringToImports = (queryString: string) => {
+    return {
+        name: 'add-query-string-to-imports',
+        enforce: 'post', // Ensure it runs after the default processes
+        generateBundle(options, bundle) {
+            for (const file in bundle) {
+                const chunk = bundle[file];
+                if (chunk.type === 'chunk') {
+                    chunk.code = chunk.code.replace(
+                        `/(import\s+.*?from\s+['"])(.*?)(['"])/g, '$1$2?${queryString}$3'`
+                    );
+                    chunk.code = chunk.code.replace(
+                        `/(export\s+.*?from\s+['"])(.*?)(['"])/g, '$1$2?${queryString}$3'`
+                    );
+                }
+
+                if (chunk.type === 'asset' && chunk.fileName.endsWith('.css')) {
+                    // Modify CSS url() references
+                    chunk.source = chunk.source.replace(/(url\(['"]?)(.*?)(['"]?\))/g, '$1$2?x-cratis-microservice=accounts$3');
+                }
+            }
+        },
+    };
+};
